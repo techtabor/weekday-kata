b2cb040be45b7435da02007c92fe6d4e476fcf50
Ildiko Czeller <czeildi@gmail.com>
2018-06-07 09:30:01 +0200

    day of week for one week

--- weekday.R
+++ weekday.R
@@ -1,5 +1,16 @@
 weekdayOfDate <- function(year, month, day) {
-    "Thursday"
+    weekdays <- c(
+        "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+        "Saturday", "Sunday"
+    )
+    weekdays[day - 3]
 }
 
 stopifnot(weekdayOfDate(2018, 6, 7) == "Thursday")
+
+stopifnot(weekdayOfDate(2018, 6, 4) == "Monday")
+stopifnot(weekdayOfDate(2018, 6, 5) == "Tuesday")
+stopifnot(weekdayOfDate(2018, 6, 6) == "Wednesday")
+stopifnot(weekdayOfDate(2018, 6, 8) == "Friday")
+stopifnot(weekdayOfDate(2018, 6, 9) == "Saturday")
+stopifnot(weekdayOfDate(2018, 6, 10) == "Sunday")
