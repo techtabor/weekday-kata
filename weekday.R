dcd07850c3f9178c21387181b6fa0c1071ac4b62
Ildiko Czeller <czeildi@gmail.com>
2018-06-07 09:38:25 +0200

    return day of any day in month

--- weekday.R
+++ weekday.R
@@ -1,13 +1,14 @@
 weekdayOfDate <- function(year, month, day) {
-    nameOfDayWithIndex(day - 3)
+    index_of_a_last_day_of_week <- 3
+    nameOfDayWithIndex((day - index_of_a_last_day_of_week) %% 7)
 }
 
 nameOfDayWithIndex <- function(index_in_week) {
-    weekdays <- c(
+    weekday_names <- c(
         "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
         "Saturday", "Sunday"
     )
-    weekdays[index_in_week]
+    weekday_names[index_in_week]
 }
 
 stopifnot(weekdayOfDate(2018, 6, 7) == "Thursday")
@@ -18,3 +19,8 @@ stopifnot(weekdayOfDate(2018, 6, 6) == "Wednesday")
 stopifnot(weekdayOfDate(2018, 6, 8) == "Friday")
 stopifnot(weekdayOfDate(2018, 6, 9) == "Saturday")
 stopifnot(weekdayOfDate(2018, 6, 10) == "Sunday")
+
+stopifnot(weekdayOfDate(2018, 6, 1) == "Friday")
+stopifnot(weekdayOfDate(2018, 6, 30) == "Saturday")
+
+
